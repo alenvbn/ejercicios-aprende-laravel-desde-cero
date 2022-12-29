a29fea2d09216a144298c3b76aec2d51f558bd1f
Alejandro Navarro <alejandro.navarro.bonilla@est.una.ac.cr>
2022-12-29 10:23:38 -0600

    Solution exercise 2

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,35 @@ Route::get('/ejercicio1', function () {
 Route::post('/ejercicio1', function () {
     return "POST OK";
 });
+
+// Ejercicio 2
+
+// a)
+
+Route::post('/ejercicio2/a', function (Request $request) {
+    return Response::json(['name' => $request->get('name'), 'description' => $request->get('description'), 'price' => $request->get('price')]);
+});
+
+// b)
+
+Route::post('/ejercicio2/b', function (Request $request) {
+    if ($request->get('price') < 0) {
+        return Response::json(['message' => "Price can't be less than 0"])->setStatusCode(422);
+    }
+    return Response::json(['name' => $request->get('name'), 'description' => $request->get('description'), 'price' => $request->get('price')]);
+});
+
+// c)
+
+Route::post('/ejercicio2/c', function (Request $request) {
+    $type_discount = $request->query('discount');
+    $discount = 0;
+    if ($type_discount == 'SAVE5') {
+        $discount = 0.05;
+    } else if ($type_discount == 'SAVE10') {
+        $discount = 0.1;
+    } else if ($type_discount == 'SAVE15') {
+        $discount = 0.15;
+    }
+    return Response::json(['name' => $request->get('name'), 'description' => $request->get('description'), 'price' => $request->get('price') - ($request->get('price') * $discount), 'discount' => $discount * 100]);
+});
